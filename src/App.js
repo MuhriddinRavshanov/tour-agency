0373ebd62f14823f68d98e728fa90f76c7d43fb7
Jalolov_Manguberdi <62201299+Developer06092000@users.noreply.github.com>
2022-05-08 22:56:18 +0500

    asd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import Header from './components/Header/Header';
-import React from 'react';
-import Republics from './components/Republics/Republics';
+import Header from "./components/Header/Header";
+import React from "react";
+import Republics from "./components/Republics/Republics";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-      <Header/>
-      <Republics/>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/republics" element={<Republics />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
