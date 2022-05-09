a09464346794cf766ebfd2e09a51c31b1cb55155
Jalolov_Manguberdi <62201299+Developer06092000@users.noreply.github.com>
2022-05-09 13:47:53 +0500

    tab update

--- src/App.js
+++ src/App.js
@@ -5,47 +5,47 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import RepGuide from "./components/RepGuide/RepGuide";
 import Footer from "./components/Footer/Footer";
 const App = () => {
-  let data = [
-    {
-      path: "",
-      name: "Central Asia",
-    },
-    {
-      path: "uzbekistan",
-      name: "Uzbekistan",
-    },
-    {
-      path: "turkmenistan",
-      name: "Turkmenistan",
-    },
-    {
-      path: "tajikistan",
-      name: "Tajikistan",
-    },
-    {
-      path: "kazakhstan",
-      name: "Kazakhstan",
-    },
-    {
-      path: "kyrgyzstan",
-      name: "Kyrgyzstan",
-    },
-  ];
-  return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Republics data={data} />
-        <Routes>
-          {data.map((item, index) => (
-            <Route path={`${item.path}`} key={index} element={<RepGuide />} />
-          ))}
-        </Routes>
-      </BrowserRouter>
-      {/* <RepGuide/> */}
-      <Footer />
-    </div>
-  );
+    let data = [
+        {
+            path: "",
+            name: "Central Asia",
+        },
+        {
+            path: "uzbekistan",
+            name: "Uzbekistan",
+        },
+        {
+            path: "turkmenistan",
+            name: "Turkmenistan",
+        },
+        {
+            path: "tajikistan",
+            name: "Tajikistan",
+        },
+        {
+            path: "kazakhstan",
+            name: "Kazakhstan",
+        },
+        {
+            path: "kyrgyzstan",
+            name: "Kyrgyzstan",
+        },
+    ];
+    return (
+        <div>
+            <BrowserRouter>
+                <Header />
+                <Republics data={data} />
+                <Routes>
+                    {data.map((item, index) => (
+                        <Route path={`${item.path}`} key={index} element={<RepGuide />} />
+                    ))}
+                </Routes>
+            </BrowserRouter>
+            {/* <RepGuide/> */}
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
--- src/components/Republics/Republics.js
+++ src/components/Republics/Republics.js
@@ -4,34 +4,34 @@ import { MyButton, MyInput } from "../MyUI/MyUI";
 import { Link } from "react-router-dom";
 
 const Republics = ({ data }) => {
-  return (
-    <div className={classes.Republics}>
-      <div className={classes.row_1}>
-        <div className={classes.col_1}>
-          <MyButton>Build your own trip</MyButton>
+    return (
+        <div className={classes.Republics}>
+            <div className={classes.row_1}>
+                <div className={classes.col_1}>
+                    <MyButton>Build your own trip</MyButton>
+                </div>
+                <div className={classes.col_2}>
+                    <MyButton>EN</MyButton>
+                    <MyButton>RU</MyButton>
+                    <MyButton>FR</MyButton>
+                    <MyButton>DE</MyButton>
+                    <MyButton>IT</MyButton>
+                    <MyButton>ES</MyButton>
+                </div>
+            </div>
+            <div className={classes.row_2}>
+                {data.map((item, index) => (
+                    <button key={index}>
+                        <Link to={`${item.path}`}>{item.name}</Link>
+                    </button>
+                ))}
+                <MyInput placeholder="Search" />
+            </div>
+            <div className={classes.row_3}>
+                <small>Countries/</small>
+            </div>
         </div>
-        <div className={classes.col_2}>
-          <MyButton>EN</MyButton>
-          <MyButton>RU</MyButton>
-          <MyButton>FR</MyButton>
-          <MyButton>DE</MyButton>
-          <MyButton>IT</MyButton>
-          <MyButton>ES</MyButton>
-        </div>
-      </div>
-      <div className={classes.row_2}>
-        {data.map((item, index) => (
-          <button key={index}>
-            <Link to={`${item.path}`}>{item.name}</Link>
-          </button>
-        ))}
-        <MyInput placeholder="Search" />
-      </div>
-      <div className={classes.row_3}>
-        <small>Countries/</small>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Republics;
